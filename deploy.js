452a6226df5a3bca28562a17aef565d31481294a
Balachandar Mutharasu <balachandar.mutharasu@gmail.com>
2025-01-22 23:15:40 +0530

    Created simple storage and initial deployment

--- deploy.js
+++ deploy.js
@@ -1,13 +1,26 @@
-async function hi() {
-  await console.log("Hi");
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+  const wallet = new ethers.Wallet(
+    "0x63eb318c8c346bffb269bae08b44de08217e0c4389a8dbac2f2431000d0c435f",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.Contract(wallet, abi, binary);
+  console.log("deploying");
+  const contract = await contractFactory.deploymentTransaction();
+  console.log(contract);
 }
 
-hi()
+main()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
-
-let variable = 5;
-console.log(variable);
