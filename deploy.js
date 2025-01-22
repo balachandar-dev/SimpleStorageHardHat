5898b0bc2377e243f66b2ce97e6bca7dcdcba88c
Balachandar Mutharasu <balachandar.mutharasu@gmail.com>
2025-01-23 00:36:54 +0530

    Updated contract to factory contract

--- deploy.js
+++ deploy.js
@@ -2,6 +2,10 @@ const ethers = require("ethers");
 const fs = require("fs-extra");
 
 async function main() {
+  const overrides = {
+    gasPrice: ethers.parseUnits("20000000000", "wei"), // Can set this >= to the number read from Ganache window
+    gasLimit: 6721975, // Use the same gasLimit as read from Ganache window (or a bit higher if still having issue)
+  };
   const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
   const wallet = new ethers.Wallet(
     "0x63eb318c8c346bffb269bae08b44de08217e0c4389a8dbac2f2431000d0c435f",
@@ -12,9 +16,9 @@ async function main() {
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
-  const contractFactory = new ethers.Contract(wallet, abi, binary);
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("deploying");
-  const contract = await contractFactory.deploymentTransaction();
+  const contract = await contractFactory.deploy(overrides);
   console.log(contract);
 }
 
